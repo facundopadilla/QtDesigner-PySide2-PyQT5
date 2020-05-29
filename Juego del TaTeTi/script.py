2ab6791be2c48f66dd06039c0e2181563bc8afac
Facu Kimbo <64610246+facundokimbo@users.noreply.github.com>
2020-05-28 23:34:21 -0300

    Add files via upload

---
+++ Juego del TaTeTi/script.py
@@ -0,0 +1,234 @@
+# Diseñado por Facundo Kimbo - https://www.github.com/facundokimbo 
+# Éste contenido es de uso libre y contiene licencia MIT, por lo tanto, no me responsabilizo de daños y prejuicios en el caso de su uso y/o modificación.
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from ui_tateti import Ui_MainWindow 
+import sys
+
+class Tateti(QtWidgets.QMainWindow):
+
+	# -- Constructor --
+
+    def __init__(self):
+
+        super(Tateti, self).__init__() # inicializo y heredo los widgets
+
+        self.tateti = Ui_MainWindow() # creo mi atributo
+
+        self.tateti.setupUi(self) # cargo mi setupUi
+
+        # -- Variables --
+
+        self.turno = 1 # si vale 1, es del jugador 1, si es 2, es del jugador 2, pero siempre empieza el jugador 1
+        self.matriz = [[None, None, None],[None, None, None],[None, None, None]] # matriz de 3x3
+        self.contador = 0 # para verificar si hay empate o no
+
+        # --- Clicked buttons ---
+
+            # >>> Matriz de Tateti <<<
+
+        self.tateti.btn1.clicked.connect(self.btn1Clicked) # cuando hago click en el boton 1 (fila 1, columna 1)
+        self.tateti.btn2.clicked.connect(self.btn2Clicked) # cuando hago click en el boton 2 (fila 1, columna 2)
+        self.tateti.btn3.clicked.connect(self.btn3Clicked) # cuando hago click en el boton 3 (fila 1, columna 3)
+        self.tateti.btn4.clicked.connect(self.btn4Clicked) # cuando hago click en el boton 4 (fila 2, columna 1)
+        self.tateti.btn5.clicked.connect(self.btn5Clicked) # cuando hago click en el boton 5 (fila 2, columna 2)
+        self.tateti.btn6.clicked.connect(self.btn6Clicked) # cuando hago click en el boton 6 (fila 2, columna 3)
+        self.tateti.btn7.clicked.connect(self.btn7Clicked) # cuando hago click en el boton 7 (fila 3, columna 1)
+        self.tateti.btn8.clicked.connect(self.btn8Clicked) # cuando hago click en el boton 8 (fila 3, columna 2)
+        self.tateti.btn9.clicked.connect(self.btn9Clicked) # cuando hago click en el boton 9 (fila 3, columna 3)
+
+            # >>> Boton de "Comenzar juego" <<<
+
+        self.tateti.btnComenzar.clicked.connect(self.comenzar)
+
+    # --- Funciones ---
+
+    def comenzar(self):
+
+        # -- Limpiar las X y O --
+        self.tateti.btn1.setText("")
+        self.tateti.btn2.setText("")
+        self.tateti.btn3.setText("")
+        self.tateti.btn4.setText("")
+        self.tateti.btn5.setText("")
+        self.tateti.btn6.setText("")
+        self.tateti.btn7.setText("")
+        self.tateti.btn8.setText("")
+        self.tateti.btn9.setText("")
+
+        # -- Limpiar matriz --
+        self.matriz = [[None, None, None],[None, None, None],[None, None, None]]
+
+        # -- Activar casillas --
+        self.tateti.btn1.setEnabled(True)
+        self.tateti.btn2.setEnabled(True) 
+        self.tateti.btn3.setEnabled(True) 
+        self.tateti.btn4.setEnabled(True) 
+        self.tateti.btn5.setEnabled(True) 
+        self.tateti.btn6.setEnabled(True) 
+        self.tateti.btn7.setEnabled(True) 
+        self.tateti.btn8.setEnabled(True) 
+        self.tateti.btn9.setEnabled(True) 
+
+        # --- Reiniciar el contador ---
+        self.contador = 0
+
+
+    def validarGanador(self):
+
+        # --- Filas y columnas en X ---
+        if((self.matriz[0][0] == "x" and self.matriz[0][1] == "x" and self.matriz[0][2] == "x") # fila 1 completa de X
+        or (self.matriz[1][0] == "x" and self.matriz[1][1] == "x" and self.matriz[1][2] == "x") # fila 2 completa de X
+        or (self.matriz[2][0] == "x" and self.matriz[2][1] == "x" and self.matriz[2][2] == "x") # fila 3 completa de X
+        or (self.matriz[0][0] == "x" and self.matriz[1][0] == "x" and self.matriz[2][0] == "x") # columna 1 completa de X
+        or (self.matriz[0][1] == "x" and self.matriz[1][1] == "x" and self.matriz[2][1] == "x") # columna 2 completa de X
+        or (self.matriz[0][2] == "x" and self.matriz[1][2] == "x" and self.matriz[2][2] == "x") # columna 3 completa de X
+        or (self.matriz[0][0] == "x" and self.matriz[1][1] == "x" and self.matriz[2][2] == "x") # diagonal de izquierda a derecha de X
+        or (self.matriz[0][2] == "x" and self.matriz[1][1] == "x" and self.matriz[2][0] == "x")): # diagonal de derecha a izquierda de X
+            self.ganador("Ganador: jugador 1") # envío el ganador
+
+        # -- Filas y columnas en O ---
+        elif((self.matriz[0][0] == "o" and self.matriz[0][1] == "o" and self.matriz[0][2] == "o") # fila 1 completa de O
+        or (self.matriz[1][0] == "o" and self.matriz[1][1] == "o" and self.matriz[1][2] == "o") # fila 2 completa de O
+        or (self.matriz[2][0] == "o" and self.matriz[2][1] == "o" and self.matriz[2][2] == "o") # fila 3 completa de O
+        or (self.matriz[0][0] == "o" and self.matriz[1][0] == "o" and self.matriz[2][0] == "o") # columna 1 completa de O
+        or (self.matriz[0][1] == "o" and self.matriz[1][1] == "o" and self.matriz[2][1] == "o") # columna 2 completa de O
+        or (self.matriz[0][2] == "o" and self.matriz[1][2] == "o" and self.matriz[2][2] == "o") # clumna 3 completa de O
+        or (self.matriz[0][0] == "o" and self.matriz[1][1] == "o" and self.matriz[2][2] == "o") # diagonal de izquierda a derecha de O
+        or (self.matriz[0][2] == "o" and self.matriz[1][1] == "o" and self.matriz[2][0] == "o")): # diagonal de derecha a izquierda de O
+            self.ganador("Ganador: jugador 2") # envío el ganador
+
+        # -- Si nadie gana --
+        elif self.contador == 9:
+            self.ganador("Empate, nadie gana.")
+
+    def ganador(self, ganador):
+        alerta = QMessageBox() # creo una caja de mensaje (QMessageBox)
+        alerta.setWindowTitle("Ganador") # le defino un título
+        alerta.setText(f"{ganador}") # le indico quien es el ganador
+        mostrar_alerta = alerta.exec_() # la ejecuto y muestro en pantalla
+
+    def btn1Clicked(self):
+        if self.turno == 1: # si es el turno del primer jugador
+            self.tateti.btn1.setText("X") # le modifico el texto del botón por una X
+            self.matriz[0][0] = "x" # añado a la matriz la X
+            self.turno = 2 # modifico el turno y ahora es del jugador 2
+        elif self.turno == 2: # si es el turno del segundo jugador
+            self.tateti.btn1.setText("O") # le modifico el texto del botón por una O
+            self.turno = 1 # modifico el turno y ahora es del jugador 1
+            self.matriz[0][0] = "o" # añado a la matriz la X
+        self.tateti.btn1.setEnabled(False) # desactivo para que no haya modificación
+        self.contador += 1 # sumo el contador
+        self.validarGanador() # valido si ganó o no
+
+    def btn2Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn2.setText("X")
+            self.matriz[0][1] = "x" 
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn2.setText("O")
+            self.matriz[0][1] = "o" 
+            self.turno = 1
+        self.tateti.btn2.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn3Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn3.setText("X")
+            self.matriz[0][2] = "x" 
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn3.setText("O")
+            self.matriz[0][2] = "o" 
+            self.turno = 1
+        self.tateti.btn3.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn4Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn4.setText("X")
+            self.matriz[1][0] = "x" 
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn4.setText("O")
+            self.matriz[1][0] = "o" 
+            self.turno = 1
+        self.tateti.btn4.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn5Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn5.setText("X")
+            self.matriz[1][1] = "x" 
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn5.setText("O")
+            self.matriz[1][1] = "o" 
+            self.turno = 1
+        self.tateti.btn5.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn6Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn6.setText("X")
+            self.matriz[1][2] = "x"
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn6.setText("O")
+            self.matriz[1][2] = "o"
+            self.turno = 1
+        self.tateti.btn6.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn7Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn7.setText("X")
+            self.matriz[2][0] = "x"
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn7.setText("O")
+            self.matriz[2][0] = "o"
+            self.turno = 1
+        self.tateti.btn7.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn8Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn8.setText("X")
+            self.matriz[2][1] = "x"
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn8.setText("O")
+            self.matriz[2][1] = "o"
+            self.turno = 1
+        self.tateti.btn8.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+    def btn9Clicked(self):
+        if self.turno == 1:
+            self.tateti.btn9.setText("X")
+            self.matriz[2][2] = "x"
+            self.turno = 2
+        elif self.turno == 2:
+            self.tateti.btn9.setText("O")
+            self.matriz[2][2] = "o"
+            self.turno = 1
+        self.tateti.btn9.setEnabled(False)
+        self.contador += 1
+        self.validarGanador()
+
+# -- Inicio de la app --
+
+app = QtWidgets.QApplication([])
+application = Tateti()
+application.show()
+sys.exit(app.exec())
---
+++ Juego del TaTeTi/ui_tateti.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'tateti.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+# Diseñado por Facundo Kimbo - https://www.github.com/facundokimbo 
+# Éste contenido es de uso libre y contiene licencia MIT, por lo tanto, no me responsabilizo de daños y prejuicios en el caso de su uso y/o modificación.
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(320, 319)
+        MainWindow.setStyleSheet("QWidget#centralwidget{\n"
+"background: #1d1d1d;\n"
+"}\n"
+"\n"
+"QPushButton{\n"
+"background-color: transparent;\n"
+"border: none;\n"
+"color: white;\n"
+"font-size: 24px;\n"
+"}\n"
+"\n"
+"QPushButton#btnComenzar{\n"
+"background: #2d89ef;\n"
+"border-radius: 4px;\n"
+"font-size: 15px;\n"
+"}\n"
+"\n"
+"QPushButton#btnComenzar:hover{\n"
+"background: #2b5797;\n"
+"}\n"
+"")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(20)
+        sizePolicy.setVerticalStretch(20)
+        font = QtGui.QFont()
+        font.setFamily("Nirmala UI")
+        font.setItalic(True)
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(110, 70, 20, 161))
+        self.line.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.line_3 = QtWidgets.QFrame(self.centralwidget)
+        self.line_3.setGeometry(QtCore.QRect(50, 110, 211, 20))
+        self.line_3.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_3.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_3.setObjectName("line_3")
+        self.line_4 = QtWidgets.QFrame(self.centralwidget)
+        self.line_4.setGeometry(QtCore.QRect(50, 170, 211, 20))
+        self.line_4.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_4.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_4.setObjectName("line_4")
+        self.line_2 = QtWidgets.QFrame(self.centralwidget)
+        self.line_2.setGeometry(QtCore.QRect(180, 70, 20, 161))
+        self.line_2.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.btn3 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn3.setGeometry(QtCore.QRect(200, 70, 51, 41))
+        self.btn3.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn3.setText("")
+        self.btn3.setObjectName("btn3")
+        self.btn2 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn2.setGeometry(QtCore.QRect(130, 70, 51, 41))
+        self.btn2.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn2.setText("")
+        self.btn2.setObjectName("btn2")
+        self.btn1 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn1.setGeometry(QtCore.QRect(60, 70, 51, 41))
+        self.btn1.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn1.setText("")
+        self.btn1.setObjectName("btn1")
+        self.btn4 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn4.setGeometry(QtCore.QRect(60, 130, 51, 41))
+        self.btn4.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn4.setText("")
+        self.btn4.setObjectName("btn4")
+        self.btn7 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn7.setGeometry(QtCore.QRect(60, 190, 51, 41))
+        self.btn7.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn7.setText("")
+        self.btn7.setObjectName("btn7")
+        self.btn8 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn8.setGeometry(QtCore.QRect(130, 190, 51, 41))
+        self.btn8.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn8.setText("")
+        self.btn8.setObjectName("btn8")
+        self.btn9 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn9.setGeometry(QtCore.QRect(200, 190, 51, 41))
+        self.btn9.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn9.setText("")
+        self.btn9.setObjectName("btn9")
+        self.btn6 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn6.setGeometry(QtCore.QRect(200, 130, 51, 41))
+        self.btn6.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn6.setText("")
+        self.btn6.setObjectName("btn6")
+        self.btn5 = QtWidgets.QPushButton(self.centralwidget)
+        self.btn5.setGeometry(QtCore.QRect(130, 130, 51, 41))
+        self.btn5.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn5.setText("")
+        self.btn5.setObjectName("btn5")
+        self.btnComenzar = QtWidgets.QPushButton(self.centralwidget)
+        self.btnComenzar.setGeometry(QtCore.QRect(90, 260, 130, 34))
+        self.btnComenzar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btnComenzar.setObjectName("btnComenzar")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "TaTeTi - Facundo Kimbo"))
+        self.btnComenzar.setText(_translate("MainWindow", "Limpiar y jugar"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
