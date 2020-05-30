8cf998c6feb99581be282c8586f6ce7ab526027f
Facu Kimbo <64610246+facundokimbo@users.noreply.github.com>
2020-05-30 18:10:32 -0300

    Add files via upload

---
+++ Editor de texto/script.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: Facundo Kimbo - https://www.github.com/facundokimbo
+# License MIT
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+from ui_editor import Ui_MainWindow
+import sys
+
+class Editor(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Editor, self).__init__()
+
+        self.editor = Ui_MainWindow()
+
+        self.editor.setupUi(self)
+
+        # -- Variables --
+
+        self.contenido = ""
+        self.ruta_del_archivo = ""
+
+        # -- Variables de CONTROL --
+        self.CONTROL_ruta_del_archivo = ""
+        self.CONTROL_contenido = ""
+        self.CONTROL_alerta1 = False
+        self.CONTROL_alerta2 = False
+
+        # -- Funciones menuBar --
+        self.editor.actionNuevo.triggered.connect(self.nuevo_archivo) # cuando hagas click en 'Nuevo' en "Abrir -> Nuevo"
+        self.editor.actionAbrir.triggered.connect(self.abrir_archivo) # cuando hagas click en 'Abrir' en "Abrir -> Abrir"
+        self.editor.actionGuardar.triggered.connect(self.guardar_archivo) # cuando hagas click en 'Guardar' en "Abrir -> Guardar"
+        self.editor.actionGuardar_como.triggered.connect(self.guardar_como) # cuando hagas click en 'Guardar como' en "Abrir -> Guardar como"
+
+        # -- Ventana de alerta ---
+
+        self.alerta = QMessageBox() # creo una caja de mensaje (QMessageBox), (def msgAlerta)
+
+    # -- Def eventos --
+
+    def nuevo_archivo(self):
+        self.CONTROL_contenido = self.editor.textEdit.toPlainText() # obtener el contenido del textEdit
+        if (self.contenido == self.CONTROL_contenido) and (self.ruta_del_archivo == self.CONTROL_ruta_del_archivo or self.ruta_del_archivo == ""):
+            self.editor.textEdit.setText("") # limpio el contenido (textEdit)
+            self.contenido = "" # libero memoria
+            self.CONTROL_contenido = "" # libero memoria del control de contenido
+        elif self.contenido != "" or self.contenido != self.CONTROL_contenido:
+            self.msgAlerta(1) # alerta del tipo 1
+            
+    def abrir_archivo(self):
+        self.CONTROL_contenido = self.editor.textEdit.toPlainText() # obtener el contenido del textEdit
+        if (self.contenido == self.CONTROL_contenido) or (self.CONTROL_alerta2 == True):
+            self.ruta_del_archivo = QFileDialog.getOpenFileName(None, "Selecciona un archivo", "", "Python (*.py);;Texto (*.txt)") # busco la dirección de donde está el archivo y filtro los archivos
+            try: 
+                self.archivo = open(f"{self.ruta_del_archivo[0]}", "r", encoding="utf-8") # como me retorna una lista, obtengo el path (ruta) y le digo que solo lo lea, y lo obtengo en utf-8
+                self.contenido = ("").join(self.archivo.readlines()) # guardo el contenido
+                self.archivo.close() # cierro el archivo para liberar memoria
+                self.editor.textEdit.setText(self.contenido) # muestro el contenido en el textEdit
+                self.CONTROL_contenido = self.contenido # hago el control de contenido
+                self.setWindowTitle(f"Editor de texto - {self.ruta_del_archivo[0]}") # le modifico el título de la ventana
+            except FileNotFoundError:
+                pass # porque si no seleccionaste nada, no tiene que pasar nada
+        elif self.CONTROL_alerta3 == False:
+            self.msgAlerta(2) # alerta del tipo 2
+
+    def guardar_archivo(self):
+        self.CONTROL_contenido = self.editor.textEdit.toPlainText() # obtener el contenido del textEdit
+        if self.ruta_del_archivo == self.CONTROL_ruta_del_archivo:
+            self.ruta_del_archivo = QFileDialog.getSaveFileName(None, "Guardar archivo", "", "Python (*.py);;Texto (*.txt)") # filtro de como guardar el archivo
+            try:
+                self.archivo = open(self.ruta_del_archivo[0], "w", encoding="utf-8") # creo el archivo
+                self.archivo.write(self.CONTROL_contenido) # añado contenido al archivo creado
+                self.archivo.close() # cierro el archivo para liberar memoria
+                self.setWindowTitle(f"Editor de texto - {self.ruta_del_archivo[0]}") # le modifico el título de la ventana
+                self.CONTROL_ruta_del_archivo = self.ruta_del_archivo # control de ruta del archivo
+            except FileNotFoundError:
+                pass # si no guardo ningun archivo, no tiene que pasar nada
+        elif self.ruta_del_archivo != "" and self.ruta_del_archivo == self.CONTROL_ruta_del_archivo:
+            try:
+                self.archivo = open(self.ruta_del_archivo[0], "w", encoding="utf-8") # creo el archivo
+                self.archivo.write(self.contenido) # añado contenido al archivo creado
+                self.archivo.close() # cierro el archivo para liberar memoria
+            except FileNotFoundError:
+                pass # si no guardo ningun archivo, no tiene que pasar nada
+    
+    def guardar_como(self):
+        self.ruta_del_archivo = QFileDialog.getSaveFileName(None, "Guardar archivo") # no le paso ningun filtro, lo puede guardar en cualquier extensión
+        try:
+            self.archivo = open(self.ruta_del_archivo[0], "w", encoding="utf-8") # creo el archivo
+            self.archivo.write(self.CONTROL_contenido) # añado contenido al archivo creado
+            self.archivo.close() # cierro el archivo para liberar memoria
+            self.setWindowTitle(f"Editor de texto - {self.ruta_del_archivo[0]}") # le modifico el título de la ventana
+            self.CONTROL_ruta_del_archivo = self.ruta_del_archivo # creo el control de la ruta
+        except FileNotFoundError:
+            pass # si no guardo ningun archivo, no tiene que pasar nada
+
+    # -- Mensajes de Alerta ---
+
+    def msgAlerta(self, numero_error):
+        if numero_error == 1:
+            self.alerta.setWindowTitle("Alerta") # le defino un título
+            self.alerta.setIcon(QMessageBox.Warning)
+            self.alerta.setText(f"¿Estás seguro que quieres crear un nuevo archivo sin guardar?. No podrás recuperar lo que haz escrito.")
+            self.alerta.setStandardButtons(QMessageBox.Yes | QMessageBox.No) # botón de YES y botón de NO
+            btnSi = self.alerta.button(QMessageBox.Yes) # Obtengo el botón YES
+            btnSi.setText("Si") # le modifico el YES por el Si
+            mostrar_alerta = self.alerta.exec_() # la ejecuto y muestro en pantalla
+            if mostrar_alerta == QMessageBox.Yes:
+                self.editor.textEdit.setText("") # le borro todo el contenido, si elige que NO, no pasa nada (no borra el texto)
+        elif numero_error == 2:
+            self.alerta.setWindowTitle("Alerta") # le defino un título
+            self.alerta.setIcon(QMessageBox.Warning) # le indico un icono de "Warning" (peligro)
+            self.alerta.setText(f"¿Estás seguro que quieres abrir un nuevo archivo sin guardar?. No podrás recuperar lo que haz escrito.")
+            self.alerta.setStandardButtons(QMessageBox.Yes | QMessageBox.No) # botón de YES y botón de NO
+            btnSi = self.alerta.button(QMessageBox.Yes) # Obtengo el botón YES
+            btnSi.setText("Si") # le modifico el YES por el Si
+            mostrar_alerta = self.alerta.exec_() # la ejecuto y muestro en pantalla
+            if mostrar_alerta == QMessageBox.Yes:
+                self.CONTROL_alerta2 = True
+                self.abrir_archivo() # va a abrir otro archivo
+
+
+# -- Iniciar la app --
+
+app = QtWidgets.QApplication([])
+application = Editor()
+application.show()
+sys.exit(app.exec())
---
+++ Editor de texto/ui_editor.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'editor.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(617, 578)
+        font = QtGui.QFont()
+        font.setFamily("Myanmar Text")
+        MainWindow.setFont(font)
+        MainWindow.setStyleSheet("*{\n"
+"color:white;\n"
+"}\n"
+"\n"
+"QTextEdit{\n"
+"font-size: 18px;\n"
+"background-color: #1d1d1d;\n"
+"color: white;\n"
+"font-family: arial;\n"
+"}\n"
+"\n"
+"QMenuBar{\n"
+"background-color: #1d1d1d;\n"
+"}\n"
+"\n"
+"QMenuBar:item{\n"
+"background-color: #1d1d1d;\n"
+"}\n"
+"\n"
+"QMenuBar:item:selected{\n"
+"background-color: #292929;\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"background-color: #1d1d1d;\n"
+"}\n"
+"\n"
+"QMenu:item:selected{\n"
+"background-color: #292929;\n"
+"}")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        self.centralwidget.setSizeIncrement(QtCore.QSize(0, 0))
+        self.centralwidget.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetMinAndMaxSize)
+        self.verticalLayout.setContentsMargins(0, -1, -1, -1)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.textEdit.sizePolicy().hasHeightForWidth())
+        self.textEdit.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("arial")
+        font.setPointSize(-1)
+        self.textEdit.setFont(font)
+        self.textEdit.setContextMenuPolicy(QtCore.Qt.PreventContextMenu)
+        self.textEdit.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.textEdit.setFrameShape(QtWidgets.QFrame.Box)
+        self.textEdit.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 617, 31))
+        self.menubar.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.menubar.setObjectName("menubar")
+        self.menuAbrir = QtWidgets.QMenu(self.menubar)
+        self.menuAbrir.setObjectName("menuAbrir")
+        self.menuAutor = QtWidgets.QMenu(self.menubar)
+        self.menuAutor.setObjectName("menuAutor")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionNuevo = QtWidgets.QAction(MainWindow)
+        self.actionNuevo.setObjectName("actionNuevo")
+        self.actionGuardar = QtWidgets.QAction(MainWindow)
+        self.actionGuardar.setObjectName("actionGuardar")
+        self.actionGuardar_como = QtWidgets.QAction(MainWindow)
+        self.actionGuardar_como.setObjectName("actionGuardar_como")
+        self.actionAbrir = QtWidgets.QAction(MainWindow)
+        self.actionAbrir.setObjectName("actionAbrir")
+        self.menuAbrir.addAction(self.actionNuevo)
+        self.menuAbrir.addSeparator()
+        self.menuAbrir.addAction(self.actionAbrir)
+        self.menuAbrir.addSeparator()
+        self.menuAbrir.addAction(self.actionGuardar)
+        self.menuAbrir.addSeparator()
+        self.menuAbrir.addAction(self.actionGuardar_como)
+        self.menuAbrir.addSeparator()
+        self.menubar.addAction(self.menuAbrir.menuAction())
+        self.menubar.addAction(self.menuAutor.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Editor de texto"))
+        self.menuAbrir.setTitle(_translate("MainWindow", "Archivo"))
+        self.menuAutor.setTitle(_translate("MainWindow", "https://www.github.com/facundokimbo"))
+        self.actionNuevo.setText(_translate("MainWindow", "Nuevo"))
+        self.actionGuardar.setText(_translate("MainWindow", "Guardar"))
+        self.actionGuardar_como.setText(_translate("MainWindow", "Guardar como"))
+        self.actionAbrir.setText(_translate("MainWindow", "Abrir"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
