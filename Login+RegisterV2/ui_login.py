2d149218ef607c927323b26e88ecef92f397ae3b
Facundo Padilla <64610246+facundopadilla@users.noreply.github.com>
2020-06-05 21:35:41 -0300

    Update ui_login.py

--- Login+RegisterV2/ui_login.py
+++ Login+RegisterV2/ui_login.py
@@ -3,7 +3,8 @@
 # Form implementation generated from reading ui file 'login.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
-#
+# @author: facundopadilla
+# @linkedin: https://www.linkedin.com/in/facundopadilla/
 # WARNING! All changes made in this file will be lost!
 
 
